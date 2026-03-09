9d2d666007c399f0b22706d33530d3ca882a37a5
Mehedi <86913265+Mehediism@users.noreply.github.com>
2026-03-09 15:55:28 +0600

    Add remote service consts

---
+++ src/AbpAuthDemo.Application.Contracts/AbpAuthDemoRemoteServiceConsts.cs
@@ -0,0 +1,7 @@
+namespace AbpAuthDemo;
+
+public static class AbpAuthDemoRemoteServiceConsts
+{
+    public const string RemoteServiceName = "AbpAuthDemo";
+    public const string ModuleName = "abpAuthDemo";
+}
